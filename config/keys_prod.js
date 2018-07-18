3a93ff82588d3ff5956daf7e583e41d3c58c3920
SamuelLJackson <severett29@gmail.com>
2018-07-18 00:37:03 -0400

    fixing db uri

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-  //mongoURI: process.env.MONGO_URI
-  mongoURI: 'mongodb://localhost:37017/test'
+  mongoURI: process.env.MONGO_URI
+  //mongoURI: 'mongodb://localhost:37017/test'
 }
