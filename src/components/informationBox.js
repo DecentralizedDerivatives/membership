2f579c1b265ad135c0fbf5945fef3e54e5664093
themandalore <fett.nicholas@yahoo.com>
2018-08-03 17:04:40 -0400

    working

--- src/components/informationBox.js
+++ src/components/informationBox.js
@@ -91,7 +91,7 @@ class InformationBox extends Component {
   async requestMembership (address) {
     var self = this
     try {
-      var memberContract = await new web3.eth.Contract(membershipAbi, '0xde545ff27a2e83e4dc7827bc926bd03a9a7e75e9')
+      var memberContract = await new web3.eth.Contract(membershipAbi, '0x620b6b6ac75ad9234eb6c533688ddd8a5948650e')
       var fee = await memberContract.methods.memberFee().call({from: address})
       var options = { from: address, value: fee }
       if (process.env.NODE_ENV === 'dev') {
